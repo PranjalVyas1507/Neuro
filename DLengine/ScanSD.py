c56cd15c7e08f82f25a4b224231462ed2650fbd4
PranjalVyas1507 <pranjalvyas.1507@gmail.com>
2022-01-11 15:40:10 +0530

    Unit test functions for structured datasets

---
+++ DLengine/ScanSD.py
@@ -0,0 +1,49 @@
+### ScanSD : Scans Structured Data for Deep learning
+# X = Input features
+# y = output feauture/ value
+
+
+import os
+import numpy as np
+import pandas as import pd
+
+
+def unittest_DS(X,y):
+    pass
+    '''
+    X ---->  input features
+    y ---->  output label
+
+
+    Read X as dataframe
+    Read Y as dataseries
+
+    Findout duplicates in X,
+    (Need a primary key for this, non repeatables)
+
+
+    Findout NaN in X,
+    Findout NaN in y
+
+
+
+
+    '''
+
+
+def feature_selection(X,y):
+    pass
+    '''
+        chisquare analysis, pearson corellation and ANOVA
+        chisquare : categorial 2 categorical
+        pearson: numerical to numerical
+        ANOVA : numerical to categorical
+    '''
+
+def numerical_feature_distribution(X):
+    pass
+    " Find out the overall distribution of datapoints over its mean"
+
+def categorical_feature_distribution(X):
+    pass
+    "Distribution of categorical datapoints"
