a1d188591f266a3b8b84336ff78fd0ee160dd563
PranjalVyas1507 <pranjalvyas.1507@gmail.com>
2022-01-19 13:53:06 +0530

    Script for selecting pre-defined optimizers

---
+++ DLengine/Optim.py
@@ -0,0 +1,23 @@
+import torch
+import torch.optim as optim
+
+
+import keras
+import keras
+
+
+
+def find_framework(fm):
+    if(fm == 'keras'):
+        return opt_keras()
+    elif(fm == 'pytorch')
+        return opt_pyt()
+
+
+
+def opt_keras():
+    pass
+
+
+def opt_pyt():
+    pass
