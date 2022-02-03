7905457ba25fce6f5a463b783e1164ce7ac28f00
PranjalVyas1507 <pranjalvyas.1507@gmail.com>
2022-02-03 14:49:32 +0530

    Added new Project Schema

--- assets/scripts/Models/DLparams.model.js
+++ assets/scripts/Models/DLparams.model.js
@@ -38,6 +38,7 @@ var NN_Schema = new mongoose.Schema({
 });
 
 var User_info = new mongoose.Schema({
+
    UserName : {type : String,
                 required : true
    },
@@ -55,6 +56,82 @@ var User_info = new mongoose.Schema({
     },*/
     NeuralNet : { type : [NN_Schema]}
 });
+
+
+
+var Model_SubDoc = new mongoose.Schema({
+    model_id : {type : String},
+    model_name : {type : String},
+    date_created : {type : Date},
+    date_updated : {type : Date},
+    framework_used : {type : String},
+    Isdeployed : {type : Boolean},
+    Isdeployed_date : {type : Date},
+    metric_used : {type : [String]},
+    train_metric : {type : Array},
+    val_metric : {type : Array},
+    train_loss : {type : Array},
+    val_loss : {type : Array},
+    hyperparamters : {type : [hyperparameters_Sub_SubDoc]},
+    Neural_Net : {type : [NeuralArchitecture_Sub_SubDoc]},
+    Learnings : {type : [Learnings_Sub_SubDoc]},
+});
+
+var Database_SubDoc = new mongoose.Schema({
+    db_id : {type : String},
+    db_name : {type : String},
+    db_location : {type : String},
+    Input_type : {type : String},
+    date_updated : {type : Date},
+    author : {type : String},
+});
+
+// var transform_SubDoc = new mongoose.Schema({});
+
+var hyperparameters_Sub_SubDoc = new mongoose.Schema({
+    epochs : {type : Number},
+    batch_size : {type : Number},
+    learning_rate : {type : Number},
+    optimizer : {type : String},
+    test_split : {type : Number},
+    validation_split : {type : Number},
+    LR_Warmup : {type : String},
+    LR_Scheduler : {type : String},
+    momentum : {type : Number},
+});
+
+
+var NeuralArchitecture_Sub_SubDoc = new mongoose.Schema({
+    layers_name : {type : [String]},
+    layer_type : {type : [String]},
+    neurons : {type : [Number]},
+    dropouts : {type : [Number]},
+    activation_function : {type : [String]},
+    weight_initializer : {type : [String]},
+    kernel_initializer : {type : [String]},
+});
+
+var Learnings_Sub_SubDoc = new mongoose.Schema({
+    weights : {type : [Number]},
+    biases : {type : [Number]},
+    hidden_weights : {type : [Number]},
+    hidden_biases : {type : [Number]},
+});
+
+
+
+var Project_doc = new mongoose.Schema({
+    project_name : {type : String, required : true},
+    project_id : {type : ObjectId},
+    date_created : {type : Date},
+    date_modified : {type : Date},
+    contributors : {type : Array},
+    lead_author : {type : String},
+    models : {type : [Model_SubDoc]},
+    databases : {type : [Database_SubDoc]},
+});
+
+
 
 
 //mongoose.model('Neural_Model',NN_Schema);
