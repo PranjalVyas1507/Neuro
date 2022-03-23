22db35a6eb4d6c7ee889704fcb8e0d4ed82023f5
PranjalVyas1507 <pranjalvyas.1507@gmail.com>
2022-03-23 18:12:24 +0530

    Added Correlation methods

--- DLengine/datascan/relations/Correlations.py
+++ DLengine/datascan/relations/Correlations.py
@@ -16,13 +16,56 @@ class Correlations:
         self.y = y
 
     def sk_ANOVA(k=None):
-        pass
+        if X.shape[1] > 5:
+            k = int(0.8*X.shape[1])
+            X = SelectKBest(score_func=f_classif,k=k).fit_transform(X, y)
 
-    def sk_pearson(self):
-        pass
+        return X
 
-    def sk_chi2(self):
-        pass
+    def sk_pearson(X,y):
+        if X.shape[1] > 5:
+            k = int(0.8*X.shape[1])
+            X = SelectKBest(score_func=f_regression,k=k).fit_transform(X, y)
+        return X
 
+    def sk_chi2(X,y):
+        if X.shape[1] > 5:
+            k = int(0.8*X.shape[1])
+            X = SelectKBest(score_func=chi2,k=k).fit_transform(X, y)
+        return X
+
+
+
+    def seperate_num_cat(X):
+        cat_column = []
+        num_column = []
+        for column in X.columns:
+            if (X[column].dtype == 'object'):
+                X[column] = X[column].astype('category')
+                X[column] = X[column].cat.codes
+                cat_column.append(column)
+            else:
+                if(X[column].dtype == 'int64'):
+                    if(pd.Series(X[column]).nunique()/pd.Series(X[column]).count() <0.003):
+                        X[column] = X[column].astype('category')
+                        X[column] = X[column].cat.codes
+                        cat_column.append(column)
+                    else:
+                        num_column.append(column)
+                else:
+                    num_column.append(column)
+
+        X_cat = X.filter(cat_column , axis=1)
+        X_num = X.filter(num_column , axis=1)
+        return X_cat, X_num
+
+
+    """
+    Func: select_num_inputs
+    Desc: Returns a dataframe consisting of the msot correlated input features
+
+    Args : Self uses input df X and output df y.
+    Returns : df
+    """    
     def select_num_inputs(self):
         pass
